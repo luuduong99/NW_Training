bfee4d29e4ad385101df828fe75d685033da39db
luuduong99 <73738252+luuduong99@users.noreply.github.com>
2023-07-31 13:02:42 +0700

    auth login

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$10$BtQC2kmGWveN.BdNpZJ4zu2DvY5Chay.P3rGe6WaOdRWPUIJxVJs2', // password
             'remember_token' => Str::random(10),
         ];
     }
